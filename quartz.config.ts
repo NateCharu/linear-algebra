a0958d17b5c2a6ce1b35d55561c67f810d0101f9
NateCharu <ncharuka@purdue.edu>
2026-05-19 05:26:16 +0700

    fix baseUrl for GitHub Pages

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://natecharu.github.io/linear-algebra",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
